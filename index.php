679160dd2540bb2e2c1d0c2993a661ba0110a7f0
Vinicius Santana <santanaviny@gmail.com>
2014-12-18 19:17:42 -0800

    Java Script Where to, Output and Syntax

---
+++ index.php
@@ -0,0 +1,39 @@
+<html>
+<head>
+    <title>Live and Learn</title>
+</head>
+<html>
+   <section>
+		<h1>Javascript</h1>
+		<h2>Where to</h2>
+		<ul>
+			<li><a href="./javascript/scriptTaginHead.html">Script Tag in Head</a></li>
+			<li><a href="./javascript/scriptTaginBody.html">Script Tag in Body</a></li>
+			<li><a href="./javascript/scriptTaginExternal.html">Script Tag in External</a></li>
+		</ul>
+		<h2>Output</h2>
+		<ul>
+			<li><a href="./javascript/windowAlert.html">Window Alert</a></li>
+			<li><a href="./javascript/documentWrite.html">Document Write</a></li>
+			<li><a href="./javascript/documentWriteAfterLoad.html">Document Write after Load</a></li>
+			<li><a href="./javascript/innerHtml.html">Inner Html</a></li>
+			<li><a href="./javascript/consoleLog.html">Console Log</a></li>
+		</ul> 
+		<h2>Syntax</h2>
+		<ul>
+			<li><a href="./javascript/programs.html">Programs</a></li>
+			<li><a href="#">text</a></li>
+			<li><a href="#">text</a></li>
+			<li><a href="#">text</a></li>
+		</ul> 	
+		<!--
+		<h2>Output</h2>
+		<ul>
+			<li><a href="#">text</a></li>
+			<li><a href="#">text</a></li>
+			<li><a href="#">text</a></li>
+			<li><a href="#">text</a></li>
+		</ul> 	
+		-->	  
+   </section>
+</html>   
